35519a062ef86128ab015c06721dc0c023f22589
limyoursun <limyoursun@gmail.com>
2022-01-14 17:21:17 +0900

    2일 차 아자아자 화이자

--- src/js/ui_common.js
+++ src/js/ui_common.js
@@ -13,48 +13,6 @@
     })
   } slideWidth();
 
-  // gnb
-  function gnb() {
-    var lm = $("#gnb");
-    lm.a = lm.find(">li>a");
-    lm.dep2 = lm.find(">li>ul");
-    lm.dep2.a = lm.dep2.find(">li>a");
-    lm.dep2.hide();
-    lm.a.each(function () {
-      if (!$(this).next().length) {
-        $(this).addClass("empty");
-      }
-      if ($(this).hasClass("on")) {
-        $(this).next("ul").show();
-      }
-    });
-    lm.a.on("click", function () {
-      lm.a.removeClass("on");
-      if ($(this).next("ul").is(":hidden")) {
-        lm.dep2.slideUp("fast");
-        $(this).next("ul").slideDown("fast");
-      } else {
-        $(this).removeClass("on");
-        $(this).next("ul").slideUp("fast");
-      }
-      // 클릭 후 슬라이드
-      var url = $(this).attr("href");
-      if ($(this).next("ul").is("ul")) {
-        $(this).addClass('on');
-        $(this).next("ul").slideDown("fast", function () {
-          document.location.href = url;
-        });
-      } else if ($(this).next("ul").not("ul")) {
-        lm.dep2.slideUp('fast');
-        $(this).addClass("on");
-        setTimeout(function () {
-          document.location.href = url;
-        }, 200);
-      }
-      return false;
-    });
-  }
-  gnb();
 
   // calendar
   $.datepicker.setDefaults({
@@ -129,4 +87,58 @@
     timepicker: false
   });
 
+
+
+
+
+  function gnb() {
+    var lm = $("#gnb");
+    lm.a = lm.find(">li>a");
+    lm.dep2 = lm.find(">li>ul");
+    lm.dep2.a = lm.dep2.find(">li>a");
+    lm.dep2.hide();
+    lm.a.each(function () {
+      if (!$(this).next().length) {
+        console.log('0');
+        $(this).addClass("empty");
+      }
+      if ($(this).hasClass("on")) {
+        console.log('1');
+        $(this).next("ul").show();
+      }
+    });
+    lm.a.on("click", function () {
+      lm.a.removeClass("on");
+      if ($(this).next("ul").is(":hidden")) {
+        console.log('2');
+        lm.dep2.slideUp("fast");
+        $(this).next("ul").slideDown("fast");
+      } else {
+        console.log('3');
+        $(this).removeClass("on");
+        $(this).next("ul").slideUp("fast");
+      }
+      
+      // 클릭 후 슬라이드
+      var url = $(this).attr("href");
+      if ($(this).next("ul").is("ul")) {
+        console.log('4');
+        $(this).addClass('on');
+        $(this).next("ul").slideDown("fast", function () {
+          document.location.href = url;
+        });
+      } else if ($(this).next("ul").not("ul")) {
+        lm.dep2.slideUp('fast');
+        console.log('5');
+        $(this).addClass("on");
+        setTimeout(function () {
+          document.location.href = url;
+        }, 200);
+      }
+      return false;
+    });
+  }
+  gnb();
 })
+
+
