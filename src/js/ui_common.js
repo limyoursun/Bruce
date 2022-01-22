4445c18d649e5fffe8de4bd5ff04c55e86dca167
sun <limyoursun@gmail.com>
2022-01-22 23:33:52 +0900

    사용자 done

--- src/js/ui_common.js
+++ src/js/ui_common.js
@@ -1,17 +1,27 @@
-  $(function () {
+$(function () {
   // 화면 크기에 따른 slide menu hide/show
   function slideWidth() {
-    $(window).on('load', function () {
+    $(window).on("load", function () {
       if ($(window).width() <= 769) {
-        $('.slide_wrap').addClass('on');
+        $(".slide_wrap").addClass("on");
       }
-    })
-    $(window).on('resize',function () {
+    });
+    $(window).on("resize", function () {
       if ($(window).width() <= 769) {
-        $('.slide_wrap').addClass('on');
+        $(".slide_wrap").addClass("on");
       }
-    })
-  } slideWidth();
+    });
+  }
+  slideWidth();
+  
+  // toast alert
+  $(".toast_alarm li")
+    .find("button")
+    .on("click", function () {
+      $(this).parents("li").fadeToggle();
+    });
+  setTimeout(() => $(".toast_alarm").fadeIn("slow"), 500);
+  setTimeout(() => $(".toast_alarm").fadeOut("slow"), 4500);
 
   // calendar
   $.datepicker.setDefaults({
@@ -22,73 +32,80 @@
     changeYear: true,
     numberOfMonths: 1,
     regional: ["ko"],
-    dateFormat: "yy-mm-dd"
+    dateFormat: "yy-mm-dd",
   });
   $("[dataformat='datepic']").datepicker({
-    buttonText: "날짜를 선택해주세요."
+    buttonText: "날짜를 선택해주세요.",
   });
   var from = $("[dataformat='from']").datepicker({
     buttonText: "시작날짜를 선택해주세요.",
     onClose: function (selectedDate) {
-      var getName = $(this).attr('name');
-      $("input[name='" + getName + "'].to").datepicker("option", "minDate", selectedDate);
-    }
+      var getName = $(this).attr("name");
+      $("input[name='" + getName + "'].to").datepicker(
+        "option",
+        "minDate",
+        selectedDate
+      );
+    },
   });
   var to = $("[dataformat='to']").datepicker({
     buttonText: "종료날짜를 선택해주세요.",
     onClose: function (selectedDate) {
-      var getName = $(this).attr('name');
-      $("input[name='" + getName + "'].from").datepicker("option", "maxDate", selectedDate);
-    }
+      var getName = $(this).attr("name");
+      $("input[name='" + getName + "'].from").datepicker(
+        "option",
+        "maxDate",
+        selectedDate
+      );
+    },
   });
 
   // pop
-  var popBtn = $('[openpop]');
-  popBtn.on('click', function () {
-    var target = $(this).attr('openpop');
-    $('#' + target).show();
-  })
-  var closePop = $('.btn_pop_close');
-  closePop.on('click', function () {
-    $(this).parents('.pop_overlay').hide();
-  })
+  var popBtn = $("[openpop]");
+  popBtn.on("click", function () {
+    var target = $(this).attr("openpop");
+    $("#" + target).show();
+  });
+  var closePop = $(".btn_pop_close");
+  closePop.on("click", function () {
+    $(this).parents(".pop_overlay").hide();
+  });
 
-  $('.con_list .more').on('click', function () {
-    $(this).toggleClass('on');
-    $(this).parent('p').nextAll('ul').slideToggle('fast');
-  })
-
+  $(".con_list .more").on("click", function () {
+    $(this).toggleClass("on");
+    $(this).parent("p").nextAll("ul").slideToggle("fast");
+  });
 
   // datetimepicker
-  $.datetimepicker.setLocale('kr');
-  $('#datetimepicker').datetimepicker({
-    minDate: 0
+  $.datetimepicker.setLocale("kr");
+  $("#datetimepicker").datetimepicker({
+    minDate: 0,
   });
 
-  $('#date_timepicker_start').datetimepicker({
+  $("#date_timepicker_start").datetimepicker({
     useCurrent: true,
-    format: 'Y/m/d',
+    format: "Y/m/d",
     onShow: function (ct) {
       this.setOptions({
-        maxDate: $('#date_timepicker_end').val() ? $('#date_timepicker_end').val() : false
-      })
+        maxDate: $("#date_timepicker_end").val()
+          ? $("#date_timepicker_end").val()
+          : false,
+      });
     },
-    timepicker: false
+    timepicker: false,
   });
 
-  $('#date_timepicker_end').datetimepicker({
-    format: 'Y/m/d',
+  $("#date_timepicker_end").datetimepicker({
+    format: "Y/m/d",
     onShow: function (ct) {
       this.setOptions({
-        minDate: $('#date_timepicker_start').val() ? $('#date_timepicker_start').val() : false
-      })
+        minDate: $("#date_timepicker_start").val()
+          ? $("#date_timepicker_start").val()
+          : false,
+      });
     },
-    timepicker: false
+    timepicker: false,
   });
-
-
-
-
 
   function gnb() {
     var lm = $("#gnb");
@@ -98,37 +115,39 @@
     lm.dep2.hide();
     lm.a.each(function () {
       if (!$(this).next().length) {
-        console.log('0');
+        console.log("0");
         $(this).addClass("empty");
       }
       if ($(this).hasClass("on")) {
-        console.log('1');
+        console.log("1");
         $(this).next("ul").show();
       }
     });
     lm.a.on("click", function () {
       lm.a.removeClass("on");
       if ($(this).next("ul").is(":hidden")) {
-        console.log('2');
+        console.log("2");
         lm.dep2.slideUp("fast");
         $(this).next("ul").slideDown("fast");
       } else {
-        console.log('3');
+        console.log("3");
         $(this).removeClass("on");
         $(this).next("ul").slideUp("fast");
       }
-      
+
       // 클릭 후 슬라이드
       var url = $(this).attr("href");
       if ($(this).next("ul").is("ul")) {
-        console.log('4');
-        $(this).addClass('on');
-        $(this).next("ul").slideDown("fast", function () {
-          document.location.href = url;
-        });
+        console.log("4");
+        $(this).addClass("on");
+        $(this)
+          .next("ul")
+          .slideDown("fast", function () {
+            document.location.href = url;
+          });
       } else if ($(this).next("ul").not("ul")) {
-        lm.dep2.slideUp('fast');
-        console.log('5');
+        lm.dep2.slideUp("fast");
+        console.log("5");
         $(this).addClass("on");
         setTimeout(function () {
           document.location.href = url;
@@ -138,6 +157,4 @@
     });
   }
   gnb();
-})
-
-
+});
